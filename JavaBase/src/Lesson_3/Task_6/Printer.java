00a98e34d59b5735d483170abee21a56dc707f58
Oleh Slyvinskyi <olegx1x1x@gmail.com>
2023-06-21 11:00:55 +0300

    Revert "Revert "Lesson 3 Homework, AnsiColor change.""
    This reverts commit d11ab7eeb6aedd0cb2a9d33adcf64e6152ca2a7c.

--- JavaBase/src/Lesson_3/Task_6/Printer.java
+++ JavaBase/src/Lesson_3/Task_6/Printer.java
@@ -4,11 +4,13 @@ import java.util.Random;
 
 class Printer {
     final String AnsiReset = "\u001b[0m";
-    final String[] ansiColors = {"\u001b[0;31m", "\u001b[0;33m", "\u001b[0;34m", "\u001b[0;35m", "\u001b[0;36m", "\u001b[0;37m"};
+    String ansiColor;
+    final
     void print(String value) {
         Random random = new Random();
-        int randomIndex = random.nextInt(this.ansiColors.length);
-        String randomColor = ansiColors[randomIndex];
-        System.out.println("\n" + randomColor + value + AnsiReset);
+        String[] ansiColors = {"\u001b[0;31m", "\u001b[0;33m", "\u001b[0;34m", "\u001b[0;35m", "\u001b[0;36m", "\u001b[0;37m"};
+        int randomIndex = random.nextInt(ansiColors.length);
+        this.ansiColor = ansiColors[randomIndex];
+        System.out.println("\n" + ansiColor + value + AnsiReset);
     }
 }
