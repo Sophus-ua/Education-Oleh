f98ef1968edced1c8f248fdf9d0b88aa6aa08295
Oleh Slyvinskyi <olegx1x1x@gmail.com>
2023-07-20 14:27:58 +0300

    Lesson_6_Annotations_Documentation homework. Some changes.

--- JavaAdvanced/src/Lesson_6_Annotations_Documentation/Task_4/MathCalculation.java
+++ JavaAdvanced/src/Lesson_6_Annotations_Documentation/Task_4/MathCalculation.java
@@ -2,7 +2,7 @@ package Lesson_6_Annotations_Documentation.Task_4;
 
 
 class MathCalculation {
-    @Math(num1 = 1, num2 = 10)
+    @Math(num1 = 100, num2 = 200)
     public void mathSum(int num1, int num2) {
         System.out.println(num1 + num2);
     }
