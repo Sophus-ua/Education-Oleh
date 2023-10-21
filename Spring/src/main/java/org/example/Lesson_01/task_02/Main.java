657b19ced2eb2f1734815dcc0983dc7d924d2078
Oleh Slyvinskyi <olegx1x1x@gmail.com>
2023-10-21 16:54:29 +0300

    Lesson 1. Some changes#1

--- Spring/src/main/java/org/example/Lesson_01/task_02/Main.java
+++ Spring/src/main/java/org/example/Lesson_01/task_02/Main.java
@@ -3,16 +3,23 @@ package org.example.Lesson_01.task_02;
 import org.example.Lesson_01.task_02.quest.CaveOfRobbers;
 import org.example.Lesson_01.task_02.quest.DragonGorge;
 import org.example.Lesson_01.task_02.quest.IngredientsForPotion;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        Knight knight = new Knight("Boris");
+        ApplicationContext context = new ClassPathXmlApplicationContext("lesson_01.xml");
+        Knight knightBoris = context.getBean("knightBoris", Knight.class);
+        CaveOfRobbers caveOfRobbers = context.getBean("CaveOfRobbers", CaveOfRobbers.class);
+        DragonGorge dragonGorge = context.getBean("DragonGorge", DragonGorge.class);
+        IngredientsForPotion ingredientsForPotion = context.getBean("IngredientsForPotion", IngredientsForPotion.class);
+
         System.out.println("___________________________________________________________________________________");
-        System.out.println(knight.doQuestAndGetResult(new IngredientsForPotion()));
+        System.out.println(knightBoris.doQuestAndGetResult(ingredientsForPotion));
         System.out.println("___________________________________________________________________________________");
-        System.out.println(knight.doQuestAndGetResult(new CaveOfRobbers()));
+        System.out.println(knightBoris.doQuestAndGetResult(caveOfRobbers));
         System.out.println("___________________________________________________________________________________");
-        System.out.println(knight.doQuestAndGetResult(new DragonGorge()));
+        System.out.println(knightBoris.doQuestAndGetResult(dragonGorge));
         System.out.println("___________________________________________________________________________________");
     }
 }
